9649465b88197d2ae775e10f49a5075a09caac6f
brennanmrose <brennan.m.rose@gmail.com>
2019-06-18 20:13:42 -0400

    Done.

--- app/models/github_repo.rb
+++ app/models/github_repo.rb
@@ -1,9 +1,8 @@
 class GithubRepo < ApplicationRecord
 
-	def initialize(args)
-		args.each do |k, v|
-			instance_variable_set('#{k}, v') unless v.nil?
-		end
+	def initialize(hash)
+		@name = hash['name']
+		@url = hash['html_url']
 	end
 
 end
